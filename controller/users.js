3f826b615bf192017c622780bf7ea57e21d1a221
NatalieFalach <natkacandy@gmail.com>
2023-04-27 23:09:25 +0300

    avatarUpload

--- controller/users.js
+++ controller/users.js
@@ -1,5 +1,9 @@
 const User = require("../models/user");
 const { ctrlWrapper } = require("../helpers");
+const path = require("path");
+const fs = require("fs").promises;
+
+const avatarDir = path.join(__dirname, "../", "public", "avatars")
 
 const updateSubscription = async (req, res) => {
   const { subscription } = req.body;
@@ -12,6 +16,21 @@ const updateSubscription = async (req, res) => {
   })
 }
 
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`
+  const resultUpload = path.join(avatarDir, filename);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename)
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL
+  })
+}
+
 module.exports = {
   updateSubscription: ctrlWrapper(updateSubscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
 }
