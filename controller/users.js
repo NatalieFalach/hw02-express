b524d5cf317017a2a3a94a834e4401f9aa3937fb
NatalieFalach <natkacandy@gmail.com>
2023-04-28 21:13:20 +0300

    avatar resize

--- controller/users.js
+++ controller/users.js
@@ -1,7 +1,8 @@
 const User = require("../models/user");
-const { ctrlWrapper } = require("../helpers");
+const { ctrlWrapper, HttpError } = require("../helpers");
 const path = require("path");
 const fs = require("fs").promises;
+const Jimp = require("jimp");
 
 const avatarDir = path.join(__dirname, "../", "public", "avatars")
 
@@ -17,11 +18,23 @@ const updateSubscription = async (req, res) => {
 }
 
 const updateAvatar = async (req, res) => {
+  if (!req.file) {
+    throw HttpError(400, 'File is missed');
+  }
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`
   const resultUpload = path.join(avatarDir, filename);
-  await fs.rename(tempUpload, resultUpload);
+
+  Jimp.read(tempUpload, (err, image) => {
+    if (err) throw err;
+    image
+      .resize(250, 250)
+      .quality(90)
+      .write(tempUpload, () => {
+        fs.rename(tempUpload, resultUpload);
+      });
+  });
   const avatarURL = path.join("avatars", filename)
   await User.findByIdAndUpdate(_id, { avatarURL });
 
